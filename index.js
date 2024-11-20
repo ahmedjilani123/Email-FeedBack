3f0e9269503f45a7debd622a8d3f771d6f8f8d4d
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-11-20 22:42:39 +0530

    add

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get('/', function(req, res){
+  res.send("welcome");
+});
 app.post('/Email',async (req, res) => {
     try {
         const { senderEmail, senderFeedback,senderName } = req.body;
