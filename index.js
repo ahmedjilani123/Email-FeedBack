065ea652d933ab1609ecd7af510fdf9587c9118d
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-11-20 23:02:40 +0530

    add

--- index.js
+++ index.js
@@ -18,57 +18,57 @@ app.get('/', function(req, res){
 });
 app.post('/Email',async (req, res) => {
     try {
-        const { senderEmail, senderFeedback,senderName } = req.body;
+        // const { senderEmail, senderFeedback,senderName } = req.body;
+res.send(req.body)
+    //     if (!senderEmail || !senderFeedback || !senderName) {
+    //         return res.status(400).send("Sender email and feedback are required");
+    //     }
 
-        if (!senderEmail || !senderFeedback || !senderName) {
-            return res.status(400).send("Sender email and feedback are required");
-        }
+    //     console.log(process.env.EMAIL, process.env.PASS);
 
-        console.log(process.env.EMAIL, process.env.PASS);
-
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASS,
-            },
-        });
+    //     const transporter = nodemailer.createTransport({
+    //         service: "gmail",
+    //         port: 465,
+    //         secure: true,
+    //         auth: {
+    //             user: process.env.EMAIL,
+    //             pass: process.env.PASS,
+    //         },
+    //     });
     
-        let portfolioLink ="https://your-portfolio-link.com";
-        const info = await transporter.sendMail({
-          from: `"Ahmed Shaikh" <${process.env.EMAIL}>`,
-          to: senderEmail,
-          subject: "Together, We Build Something Better – Grateful for Your Valuable Feedback!",
-          text: `Dear ${senderName},\n\nThank you for taking the time to explore my portfolio and share your valuable insights. Your feedback is not just appreciated – it’s invaluable, as it inspires me to strive for even greater heights.\n\nI’m truly grateful for your support and look forward to staying connected.\n\n If you’d like to revisit my portfolio or see updates, feel free to check it out here: ${portfolioLink}\n\nBest regards,\nAhmed Shaikh`,
-          html: `
-              <p>Dear ${senderName},</p>
-              <p>Thank you for taking the time to explore my portfolio and share your valuable insights. Your feedback is not just appreciated – it’s invaluable, as it inspires me to strive for even greater heights.</p>
-              <p>I’m truly grateful for your support and look forward to staying connected.<br><br> If you’d like to revisit my portfolio or see updates, feel free to check it out here: <a href="${portfolioLink}" target="_blank">${portfolioLink}</a></p>
-              <p>Best regards,<br>Ahmed Shaikh</p>
-          `,
-      });
-      var date =new Date()
-    // setTimeout(async()=>{
-    //   const infos = await transporter.sendMail({
-    //     from: `"Ahmed Shaikh" <${process.env.EMAIL}>`,
-    //     to: 'shaikhaj7860@gmail.com',
-    //     subject: `Feedback by ${senderName}`,
-    //     text: `Dear Ahmed Sir,\n Date:${date}\n Name: ${senderName}\n Email:${senderEmail}\n Message:${senderFeedback}\n\nBest regards,\nAhmed Shaikh`
-    //     ,   html: `
-    //         <p>Dear Ahmed Sir,</p>
-    //         <p>Date:${date}</p>
-    //         <p>Name:${senderName}</p>
-    //         <p>Email:${senderEmail}</p>
-    //         <p>Message:${senderFeedback}</p>
-    //         <p>Best regards,<br>Ahmed Shaikh</p>
-    //     `,
-    // });
-    // },1000)
+    //     let portfolioLink ="https://your-portfolio-link.com";
+    //     const info = await transporter.sendMail({
+    //       from: `"Ahmed Shaikh" <${process.env.EMAIL}>`,
+    //       to: senderEmail,
+    //       subject: "Together, We Build Something Better – Grateful for Your Valuable Feedback!",
+    //       text: `Dear ${senderName},\n\nThank you for taking the time to explore my portfolio and share your valuable insights. Your feedback is not just appreciated – it’s invaluable, as it inspires me to strive for even greater heights.\n\nI’m truly grateful for your support and look forward to staying connected.\n\n If you’d like to revisit my portfolio or see updates, feel free to check it out here: ${portfolioLink}\n\nBest regards,\nAhmed Shaikh`,
+    //       html: `
+    //           <p>Dear ${senderName},</p>
+    //           <p>Thank you for taking the time to explore my portfolio and share your valuable insights. Your feedback is not just appreciated – it’s invaluable, as it inspires me to strive for even greater heights.</p>
+    //           <p>I’m truly grateful for your support and look forward to staying connected.<br><br> If you’d like to revisit my portfolio or see updates, feel free to check it out here: <a href="${portfolioLink}" target="_blank">${portfolioLink}</a></p>
+    //           <p>Best regards,<br>Ahmed Shaikh</p>
+    //       `,
+    //   });
+    //   var date =new Date()
+    // // setTimeout(async()=>{
+    // //   const infos = await transporter.sendMail({
+    // //     from: `"Ahmed Shaikh" <${process.env.EMAIL}>`,
+    // //     to: 'shaikhaj7860@gmail.com',
+    // //     subject: `Feedback by ${senderName}`,
+    // //     text: `Dear Ahmed Sir,\n Date:${date}\n Name: ${senderName}\n Email:${senderEmail}\n Message:${senderFeedback}\n\nBest regards,\nAhmed Shaikh`
+    // //     ,   html: `
+    // //         <p>Dear Ahmed Sir,</p>
+    // //         <p>Date:${date}</p>
+    // //         <p>Name:${senderName}</p>
+    // //         <p>Email:${senderEmail}</p>
+    // //         <p>Message:${senderFeedback}</p>
+    // //         <p>Best regards,<br>Ahmed Shaikh</p>
+    // //     `,
+    // // });
+    // // },1000)
       
-        console.log(info);
-        res.send("Thank-you email successfully sent");
+    //     console.log(info);
+    //     res.send("Thank-you email successfully sent");
     } catch (error) {
         console.error(error);
       
