e20fce05f2d76c38580e1a68107c6e72ae80d9b3
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-11-21 12:40:16 +0530

    add url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const corsOptions = {
-  origin: '*', 
+  origin: 'https://ahmedshaikhcv.netlify.app', 
   methods: 'GET,POST'
 };
 
